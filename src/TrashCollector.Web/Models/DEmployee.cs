9712ac302f70179292ea67344838d0bfdd2c14a2
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-02 16:01:34 -0500

    Added Employee Model and its properties

---
+++ src/TrashCollector.Web/Models/DEmployee.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TrashCollector.Web.Models
+{
+    [Table("Employee")]
+    public class DEmployee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ZipCode { get; set; }
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
